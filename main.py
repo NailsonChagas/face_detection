a56e4b6935781b6865d99dab531b7b7a89236e03
Nailson Chagas <chagasnailson@gmail.com>
2023-01-28 20:59:51 -0300

    feat: lógica do banco

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import sqlite3
 DB_CONNECTION = sqlite3.connect('./db/users.db')
+DB_CURSOR = DB_CONNECTION.cursor()
 
 def main():
     pass
 
 if __name__ == "__main__":
+    DB_CURSOR.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            ultimoAcesso DATETIME
+    );
+    """)
     main()
     DB_CONNECTION.close()
