39a7dd9a0904acb5c4ff4143e4e52b1b42f818ce
Nailson Chagas <chagasnailson@gmail.com>
2023-01-29 10:42:05 -0300

    feat: lendo webcam

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import sqlite3
+import cv2
 
 DB_CONNECTION = sqlite3.connect('./db/cv.db')
 DB_CURSOR = DB_CONNECTION.cursor()
 
 def main():
-    pass
+    cam = cv2.VideoCapture(0)
+    width = 500
+    height = 500
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
+    if not cam.isOpened():
+        raise IOError("Cannot open webcam")
+
+    while True:
+        ret, frame = cam.read()
+        cv2.imshow('WEBCAM', frame)
+
+        c = cv2.waitKey(1)
+        if c == 27:
+            break
+
+    cam.release()
+    cv2.destroyAllWindows()
+    
 if __name__ == "__main__":
     DB_CURSOR.execute("""
     CREATE TABLE IF NOT EXISTS users (
@@ -17,11 +36,11 @@ if __name__ == "__main__":
     DB_CURSOR.execute("""
     CREATE TABLE IF NOT EXISTS faces (
             user_id INTEGER,
-            imagePath VARCHAR(100),
+            encodePath VARCHAR(100),
             FOREIGN KEY(user_id) REFERENCES users(id)
     );
     """)
 
     main()
-    
+
     DB_CONNECTION.close()
